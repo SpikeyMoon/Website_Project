8a9d6fac7cf7445d1a9f03945818543136936641
SpikeyMoon <lurkerunknown@gmail.com>
2019-06-12 13:01:04 +0700

    0.0.0.1

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
 $(function () {
-    
+
+    console.log("Ready!");
+    var scrolledDown = 0;
+    var scrolledUp = 0;
+    $(window).bind('mousewheel DOMMouseScroll', function (event) {
+        var obj = $(".current");
+        if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+            // scroll up
+            if (obj.prev().is('div')) {
+                scrolledUp++;
+                scrolledDown = 0;
+                // console.log("This happened");
+                if (scrolledUp == 4) {
+                    obj.prev().addClass("current");
+                    obj.prev().removeClass("up");
+                    obj.removeClass("current");
+                    obj.addClass("down");
+                    scrolledUp = 0;
+                }
+            }
+        }
+        else {
+            // scroll down
+            if (obj.next().is('div')) {
+                scrolledDown++;
+                scrolledUp = 0;
+                // console.log("That happened");
+                if (scrolledDown == 4) {
+                    // var obj = $(".current");
+                    obj.next().addClass("current");
+                    obj.next().removeClass("down");
+                    obj.removeClass("current");
+                    obj.addClass("up");
+                    scrolledDown = 0;
+                }
+            }
+        }
+    });
+
+
 });
